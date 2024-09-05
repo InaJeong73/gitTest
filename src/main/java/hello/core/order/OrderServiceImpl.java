1eabc1d15568228bc29656b38531218fbc1a05a1
JeongIna <jeongina89@gmail.com>
2024-09-05 13:57:55 +0900

    first commit

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,15 +11,40 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+//    @Autowired <-필드 주입
+    private  MemberRepository memberRepository;
+//    @Autowired
+    private  DiscountPolicy discountPolicy;
 
-    @Autowired
+
+    //필드값을 setter함수를 통해서 보통 수정하는데, 함수에 @Autowired를 붙이면 자동 주입됌.
+//    @Autowired <- 수정자 주입
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        System.out.println("discountPolicy2 = " + discountPolicy);
+//        this.discountPolicy = discountPolicy;
+//    }
+//
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        System.out.println("memberRepository2 = " + memberRepository);
+//        this.memberRepository = memberRepository;
+//    }
+
+    //    생성자 주입, 생성자가 하나 있으면 Autowired 애노테이션 없이도 자동으로 주입시켜줌.
+//    @Autowired <- 생성자 주입
+//    생성자 주입이 선행된 후에 수정자나 함수 주입이 진행됌.
     public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy discountPolicy){
+        System.out.println("memberRepository1 = " + memberRepository);
+        System.out.println("discountPolicy1 = " + discountPolicy);
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
     }
 
+    @Autowired//<-일반 메서드 주입
+    public void init(MemberRepository memberRepository,DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member=memberRepository.findbyId(memberId);
--- src/test/java/scan/AutoAppConfigTest.java
+++ src/test/java/scan/AutoAppConfigTest.java
@@ -3,6 +3,7 @@ package scan;
 
 import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
+import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -17,5 +18,8 @@ public class AutoAppConfigTest {
 
         MemberService memberService=ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl orderService=ac.getBean("orderServiceImpl", OrderServiceImpl.class);
+        System.out.println("orderService.getMemberRepository() : "+orderService.getMemberRepository());
     }
 }
